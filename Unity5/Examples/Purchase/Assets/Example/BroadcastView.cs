4b531f6a41e653ef174ea55ba0f9e1cbdd9a0a5f
Denis Sachkov <>
2019-08-09 16:01:19 +0300

    version 2.0.28

---
+++ Unity5/Examples/Purchase/Assets/Example/BroadcastView.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BroadcastView : MonoBehaviour
+{
+    
+    
+    private static AndroidJavaObject gameofwhales = null;
+    
+    void Start()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+    using(var pluginClass = new AndroidJavaClass("com.gameofwhales.unityplugin.GameOfWhalesProxy"))
+    {
+        gameofwhales = pluginClass.CallStatic<AndroidJavaObject>("instance");
+    }
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnInitRequestClicked()
+    {
+        gameofwhales.Call("test_BroadcastInitRequest");
+    }
+
+    public void OnUserRequestClicked()
+    {
+        gameofwhales.Call("test_BroadcastUserStatusRequest");
+    }
+
+
+    public void OnAdShowRequestClicked()
+    {
+        gameofwhales.Call("test_BroadcastShowAdRequest");
+    }
+}
---
+++ Unity5/Examples/Purchase/Assets/Example/TextText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Net.Mime;
+
+public class TextText : MonoBehaviour
+{
+	public Text text;
+	private DateTime finishedAt;
+	// Use this for initialization
+	void Start () {
+		finishedAt = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+		long tt = Convert.ToInt64("1544778606647");
+		finishedAt = finishedAt.AddMilliseconds(tt);
+		//finishedAt = finishedAt.AddMilliseconds((long) 1544778606647);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DateTime endTime = finishedAt;
+		DateTime StartTime = GameOfWhales.GetServerTime();
+		TimeSpan t = endTime - StartTime;
+		
+		string tt = t.TotalSeconds.ToString();
+
+		tt += "\n ticks: " + (endTime.Ticks - StartTime.Ticks).ToString();
+		text.text = tt;
+	}
+}
